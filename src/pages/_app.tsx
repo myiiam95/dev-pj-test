27a671562f60944be0c89cdc8cae669c78db30be
iiamm <iamm.byi@gmail.com>
2025-08-15 01:51:09 +0700

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-poppins',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
---
+++ src/pages/payment-done.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+const PaymentDone = () => {
+  return (
+    <main className="h-screen flex items-center justify-center bg-gray-50">
+        <div className="content-center flex flex-col justify-center not-[]:min-h-screen">
+            <div className="">
+                <Image 
+                src="/images/img-payment-done.png" 
+                alt="Payment Success"
+                width={809}
+                height={480}
+                />
+            </div>
+            <div className="title-pay text-center mt-7 pt-4 tracking-normal">
+                <h1 className="txt-title text-5xl">Booking Successfully completed</h1>
+                <span className="text-xl">Your trip schedule is ready,please check under profile.</span>
+                <div>
+                    <span>Home</span>
+                </div>
+            </div>      
+        </div>
+    </main>
+  )
+}
+
+export default PaymentDone
